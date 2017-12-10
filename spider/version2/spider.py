0c31b8d3b475f281cd1a7aa2a9eac3035762c089
YanPanMichael <yanpancsu@icloud.com>
2017-12-11 01:08:18 +0800

    save

--- spider/version2/spider.py
+++ spider/version2/spider.py
@@ -11,12 +11,19 @@ import re
 # print req.headers.values()
 
 def get_proxy_from_cnpproxy():
-    parten = re.compile(r'''<script src="https://(.+?).bdstatic.com/5aV1bjqh_Q23odCf/static/(.+?)/js/(.+?).js"></script>''')
-    target = "http://www.baidu.com"
-    req = urllib2.urlopen(target)
-    result = req.read()
-    print result
-    matchs = parten.findall(result)
-    print matchs
+    try:
+        p1 = re.compile(r'''"(.+?)": "http://y0.ifengimg.com/base/origin/(.+?).min"''')
+        p2 = re.compile(r'''<li><a href="http://(.+?)/" target="_blank">(.+?)</a></li>''')
+        target = "http://news.ifeng.com/a/20171210/54042354_0.shtml?_zbs_baidu_news#p=1"
+        req = urllib2.urlopen(target)
+        result = req.read()
+        matchs = re.findall(p2, result)
+        for item in matchs:
+            print str(item)
+    except urllib2.URLError, e:
+        if hasattr(e,"code"):
+            print e.code
+        if hasattr(e,"reason"):
+            print e.reason
 
 get_proxy_from_cnpproxy()
